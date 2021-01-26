02ff8ad6b0b6331fde1e51da382f1a20c4e5d762
David Butler <butler.david1906@gmail.com>
2021-01-26 16:19:21 -0500

    Exercise2

--- Selection statement/Program.cs
+++ Selection statement/Program.cs
@@ -6,38 +6,65 @@ namespace Selectionstatement
     {
         static void Main(string[] args)
         {
+            string subject = "C#";
+            Console.WriteLine("Choose a subject");
+            string userSubject = Console.ReadLine();
 
-            int favoriteNumber = 15;
-
-            Console.WriteLine("In this game you must guess what my favorite number is. See if you can correctly guess my favorite number");
-            int userResponse = int.Parse(Console.ReadLine());
-
-                if (userResponse < favoriteNumber)
+            switch  (userSubject)
             {
-                Console.WriteLine("Your guess is too low, please try again");
-            }
+                case "C#":
+                    Console.WriteLine("You are in the correct class");
+                    break;
+                case "Python":
+                    Console.WriteLine("C# works better than Python");
+                    break;
+                default;
+                    Console.WriteLine("Coding rules here at TrueCoders.");
+                    break;
 
-
-                else if (userResponse > favoriteNumber) 
-            {
-                Console.WriteLine("Your guess is too high, please try again");
-            }
-
-                else if (userResponse == favoriteNumber) 
-            {
-                Console.WriteLine("You are spot on good friend.");
-            }
-
-                else
-            {
-                Console.WriteLine("Nevermind");
             }
         }
         
+        
+        
+        
+        
+        
+        {
+
+            //int favoriteNumber = 15;
+
+            //Console.WriteLine("In this game you must guess what my favorite number is. See if you can correctly guess my favorite number");
+            //int userResponse = int.Parse(Console.ReadLine());
+
+            //    if (userResponse < favoriteNumber)
+            {
+              //  Console.WriteLine("Your guess is too low, please try again");
+            }
+
+
+                //else if (userResponse > favoriteNumber) 
+            {
+                //Console.WriteLine("Your guess is too high, please try again");
+            }
+
+                //else if (userResponse == favoriteNumber) 
+            {
+                //Console.WriteLine("You are spot on good friend.");
+            }
+
+                //else
+            {
+                //Console.WriteLine("Nevermind");
+            }
+        }
+     
     }
         
 }
-            
+
+                          
+                      
             
 
 
