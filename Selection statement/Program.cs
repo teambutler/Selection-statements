9a0ea3fb085f375bc8bbb72c343b247c33ccb083
David Butler <butler.david1906@gmail.com>
2021-01-26 15:48:51 -0500

    selection exercise

--- Selection statement/Program.cs
+++ Selection statement/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 
-namespace Selection_statement
+namespace Selectionstatement
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int favoriteNumber = 15;
+
+            Console.WriteLine("In this game you must guess what my favorite number is. See if you can correctly guess my favorite number");
+            int userResponse = int.Parse(Console.ReadLine());
+
+                if (userResponse < favoriteNumber)
+            {
+                Console.WriteLine("Your guess is too low, please try again");
+            }
+
+
+                else if (userResponse > favoriteNumber) 
+            {
+                Console.WriteLine("Your guess is too high, please try again");
+            }
+
+                else if (userResponse == favoriteNumber) 
+            {
+                Console.WriteLine("You are spot on good friend.");
+            }
+
+                else
+            {
+                Console.WriteLine("Nevermind");
+            }
         }
+        
     }
+        
 }
+            
+            
+
+
+
+
+
+
+
+            
+        
